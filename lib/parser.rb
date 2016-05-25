3731b31c034dd539fcd2e8f8ee1f3e14141dbc2e
Oren Kanner <okanner@gmail.com>
2016-05-25 05:18:37 -0400

    finish parser class

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,31 @@
 class Parser
+  SEPARATOR_REGEX = /[-+|]+/
 
+  def initialize(game_str:)
+    @game_str = game_str
+  end
+
+  def self.parse(game_str:)
+    new(game_str: game_str).parse
+  end
+
+  def parse
+    lines.map { |line| line_to_array(line) }
+  end
+
+  private
+
+  attr_accessor :game_str
+
+  def lines
+    clean_str.split("\n").reject(&:empty?)
+  end
+
+  def clean_str
+    game_str.gsub(SEPARATOR_REGEX, "")
+  end
+
+  def line_to_array(line)
+    line.split(" ").map(&:to_i)
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,55 @@
 require_relative "../lib/parser.rb"
 
 describe Parser do
-end
+  it "returns a grid of integers from a space-delimited string" do
+    str = <<PUZZLE
+1 2 3
+4 5 6
+7 8 9
+PUZZLE
 
+    result = Parser.parse(game_str: str)
+
+    expect(result).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  end
+
+  it "ignores vertical separators" do
+    str = <<PUZZLE
+1 |2 |3
+4 |5 |6
+7 |8 |9
+PUZZLE
+
+    result = Parser.parse(game_str: str)
+
+    expect(result).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  end
+
+  it "ignores horizontal separators" do
+    str = <<PUZZLE
+1 2 3
+------
+4 5 6
+------
+7 8 9
+PUZZLE
+
+    result = Parser.parse(game_str: str)
+
+    expect(result).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  end
+
+  it "ignores combined separators" do
+    str = <<PUZZLE
+1 |2 |3
+--+--+--
+4 |5 |6
+--+--+--
+7 |8 |9
+PUZZLE
+
+    result = Parser.parse(game_str: str)
+
+    expect(result).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  end
+end
