4e81a5b09292ac3da63e80e728f59b8178bbd4b6
Oren Kanner <okanner@gmail.com>
2016-05-25 13:28:38 -0400

    complete Validator class

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,4 +1,12 @@
+require_relative "./board.rb"
+require_relative "./group_validator.rb"
+require_relative "./parser.rb"
+
 class Validator
+  VALID_MSG = "This sudoku is valid."
+  INVALID_MSG = "This sudoku is invalid."
+  INCOMPLETE_MSG = "This sudoku is valid, but incomplete."
+
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
   end
@@ -8,14 +16,33 @@ class Validator
   end
 
   def validate
-    # Start creating your solution here.
-    #
-    # It's likely that you'll want to have many more classes than this one that
-    # was provided for you. Don't be hesistant to extract new objects (and
-    # write tests for them).
+    return INVALID_MSG unless groups_valid?
+    return INCOMPLETE_MSG unless groups_complete?
+    VALID_MSG
   end
 
   private
 
   attr_accessor :puzzle_string
+
+  def groups_valid?
+    groups.all? { |group| GroupValidator.validate(group: group) }
+  end
+
+  def groups_complete?
+    groups.all? { |group| GroupValidator.check_missing(group: group) }
+  end
+
+  def groups
+    board.rows + board.cols + board.subgroups
+  end
+
+  def board
+    @board ||= Board.new(grid: grid)
+  end
+
+  def grid
+    @grid ||= Parser.parse(game_str: puzzle_string)
+  end
+
 end
